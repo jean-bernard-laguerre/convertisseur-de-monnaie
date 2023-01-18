c6419ccebf1c33b5e6fc18aabe8199f1430bd685
Gibet13 <48248176+Gibet13@users.noreply.github.com>
2023-01-18 23:52:53 +0100

    interface

--- convertisseur.py
+++ convertisseur.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter.messagebox import *
 
 fenetre = Tk()
+fenetre.title("Convertisseur")
 
 liste_devise = ["Euro", "Dollar", "Livre", "Yen"]
 dict_taux = {"Euro":1.00, "Dollar":1.08, "Livre":0.89, "Yen":139.21}
@@ -19,9 +20,10 @@ def Convertir():
 
     try:
         somme_depart = float(montant.get())
-        somme_finale = (somme_depart*(dict_taux["Euro"]/dict_taux[monnaie_Depart.get()]))*dict_taux[monnaie_Fin.get()]
+        somme_finale = (somme_depart * (dict_taux["Euro"] / dict_taux[monnaie_Depart.get()])) * dict_taux[monnaie_Fin.get()]
         resultat.insert(0, f"{somme_finale:.2f}")
 
+        #Ajout dans l'historique
         hist = f"{somme_depart:.2f} {monnaie_Depart.get()} = {somme_finale:.2f} {monnaie_Fin.get()}"
         lbl_hist = Label(conv_hist, text=hist)
         lbl_hist.pack()
@@ -33,6 +35,7 @@ def Convertir():
 def Ajout_Devise():
 
     try:
+        #Ajoute la devise si elle n'est pas dans la liste
         if not dict_taux[nom_devise.get()]:
             
             liste_devise.append(nom_devise.get())
@@ -41,6 +44,7 @@ def Ajout_Devise():
             Monnaie1.destroy()
             Monnaie2.destroy()
             
+            #Nouveau option menu avec liste a jour
             new_Monnaie1 = OptionMenu(conv_main, monnaie_Depart, *liste_devise)
             new_Monnaie1.config(width=24)
             new_Monnaie1.grid(row=1, column=1)
@@ -48,6 +52,10 @@ def Ajout_Devise():
             new_Monnaie2 = OptionMenu(conv_main, monnaie_Fin, *liste_devise)
             new_Monnaie2.config(width=24)
             new_Monnaie2.grid(row=2, column=1)
+        
+        #Modifie le taux si la devise est presente 
+        else:
+            dict_taux[nom_devise.get()] = float(valeur_devise.get())
 
     except:
         showerror('Erreur Ajout', 'Champ(s) invalide(s)')
@@ -55,22 +63,24 @@ def Ajout_Devise():
 
 # Menu de conversion
 
-conv_main = LabelFrame(fenetre, text="Convertisseur de monnaie", pady=10)
-conv_main.pack(fill="both", expand="yes")
+conv_main = Frame(fenetre, pady=10)
+conv_main.pack(anchor="center")
 
-lbl_Montant = Label(conv_main, text="Montant :").grid(row=0, column=0)
+conv_titre = Label(conv_main, text="Conversion", font=(16)).grid(row=0, column=0, columnspan=2, pady=10)
+
+lbl_Montant = Label(conv_main, text="Montant :").grid(row=1, column=0)
 montant = Entry(conv_main, width=30)
-montant.grid(row=0, column=1)
+montant.grid(row=1, column=1)
 
-lbl_Monnaie_init = Label(conv_main, text="De :").grid(row=1, column=0)
+lbl_Monnaie_init = Label(conv_main, text="De :").grid(row=2, column=0)
 Monnaie1 = OptionMenu(conv_main, monnaie_Depart, *liste_devise)
 Monnaie1.config(width=24)
-Monnaie1.grid(row=1, column=1)
+Monnaie1.grid(row=2, column=1)
 
-lbl_Monnaie_conv = Label(conv_main, text="A :").grid(row=2, column=0)
+lbl_Monnaie_conv = Label(conv_main, text="A :").grid(row=3, column=0)
 Monnaie2 = OptionMenu(conv_main, monnaie_Fin, *liste_devise)
 Monnaie2.config(width=24)
-Monnaie2.grid(row=2, column=1)
+Monnaie2.grid(row=3, column=1)
 
 btn_convertir = Button(conv_main, text="Convertir", command=Convertir, width= 25).grid(row=4, column=1, padx=10, pady= 10)
 
@@ -80,22 +90,26 @@ resultat.grid(row=5, column=1)
 
 # Ajout de devises
 
-ajt_devise = LabelFrame(fenetre, text="Ajout de devises", pady=10)
-ajt_devise.pack(fill="both", expand="yes")
+ajt_devise = Frame(fenetre, pady=10)
+ajt_devise.pack(anchor="center")
 
-lbl_nom_devise = Label(ajt_devise, text="Nom :").grid(row=0, column=0)
+devise_titre = Label(ajt_devise, text="Ajout de devise", font=(16)).grid(row=0, column=0, columnspan=2, pady=10)
+
+lbl_nom_devise = Label(ajt_devise, text="Nom :").grid(row=1, column=0)
 nom_devise = Entry(ajt_devise, width=30)
-nom_devise.grid(row=0, column=1)
+nom_devise.grid(row=1, column=1)
 
-lbl_valeur_devise = Label(ajt_devise, text="Taux :").grid(row=1, column=0)
+lbl_valeur_devise = Label(ajt_devise, text="Taux :").grid(row=2, column=0)
 valeur_devise = Entry(ajt_devise, width=30)
-valeur_devise.grid(row=1, column=1)
+valeur_devise.grid(row=2, column=1)
 
 btn_ajout = Button(ajt_devise, text="Ajouter", command=Ajout_Devise, width= 25).grid(row=2, column=1, padx=10, pady= 10)
 
 # Historique des conversions
 
-conv_hist = LabelFrame(fenetre, text="Historique")
-conv_hist.pack(fill="both", expand="yes")
+conv_hist = Frame(fenetre, pady=10)
+conv_hist.pack(anchor="center")
+
+hist_titre = Label(conv_hist, text="Historique", font=(16)).pack(pady=10)
 
 fenetre.mainloop()
